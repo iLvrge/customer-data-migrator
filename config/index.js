590a1217ed61d01a0364f8a5b8a73594b8fca806
Vivek Kapoor <er.vivek2512@gmail.com>
2025-12-27 12:33:13 +0530

    update the index.js

--- config/index.js
+++ config/index.js
@@ -3,89 +3,89 @@ const moment = require('moment');
 const Op = Sequelize.Op;
 
 
-const business = new Sequelize('db_business', 'db_user_all', 'wDv%5tgn0O0kMkMN', {
-  host: '167.172.195.92',
+const business = new Sequelize(process.env.DATABASE_BUSINESS, process.env.USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
 });
 
 
-const resourcesRaw = new Sequelize('db_uspto', 'db_user_all', 'wDv%5tgn0O0kMkMN', {
-    host: '167.172.195.92',
-    dialect: 'mysql',
-    operatorsAliases: Op,
-   /*
-    pool: {
-      max: 1000,
-      min: 0,
-      acquire: 1000000,
-      idle: 10000
-    }*/
-});
-
-
-const resources = new Sequelize('db_patent_application_bibliographic_new', 'db_user_all', 'wDv%5tgn0O0kMkMN', {
-    host: '167.172.195.92',
-    dialect: 'mysql',
-    operatorsAliases: Op,
-   /*
-    pool: {
-      max: 1000,
-      min: 0,
-      acquire: 1000000,
-      idle: 10000
-    }*/
-});
-
-const application = new Sequelize('db_new_application', 'db_user_all', 'wDv%5tgn0O0kMkMN', {
-  host: '167.172.195.92',
+const resourcesRaw = new Sequelize(process.env.DATABASE_RAW, process.env.USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
 });
 
 
-const applicationBibliograhic = new Sequelize('db_patent_grant_bibliographic', 'db_user_all', 'wDv%5tgn0O0kMkMN', {
-  host: '167.172.195.92',
+const resources = new Sequelize(process.env.DATABASE_APPLICATION_BIBLIO_NEW, process.env.USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
 });
 
-const applicationGrant = new Sequelize('db_patent_application_bibliographic', 'db_user_all', 'wDv%5tgn0O0kMkMN', {
-  host: '167.172.195.92',
+const application = new Sequelize(process.env.DATABASE_APPLICATION_NEW, process.env.USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
 });
 
-const applicationPED = new Sequelize('db_patent_examiner_data', 'db_user_all', 'wDv%5tgn0O0kMkMN', {
-  host: '167.172.195.92',
+
+const applicationBibliograhic = new Sequelize(process.env.DATABASE_APPLICATION_BIBLIO, process.env.USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  operatorsAliases: Op,
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
+});
+
+const applicationGrant = new Sequelize(process.env.DATABASE_GRANT_BIBLIO, process.env.USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  operatorsAliases: Op,
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
+});
+
+const applicationPED = new Sequelize(process.env.DATABASE_PATENT_EXAMINER_DATA, process.env.USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   operatorsAliases: Op,
   pool: {
@@ -97,61 +97,61 @@ const applicationPED = new Sequelize('db_patent_examiner_data', 'db_user_all', '
 });
 
 
-const inventorDB = new Sequelize('db_inventor', 'db_user_inventor', 'wDv%5tgn0O0kMkMN', {
+const inventorDB = new Sequelize('db_inventor', 'db_user_inventor', process.env.PASSWORD, {
   host: '165.232.146.68',
   dialect: 'mysql',
   operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
 });
 
 
 
 const biblioGrant = new Sequelize(process.env.DATABASE_GRANT_BIBLIO, process.env.USER, process.env.PASSWORD, {
-  host: '167.172.195.92',
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000, 
-    idle: 10000
-  }*/
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000, 
+     idle: 10000
+   }*/
 });
 
 const biblioApplication = new Sequelize(process.env.DATABASE_APPLICATION_BIBLIO, process.env.USER, process.env.PASSWORD, {
-  host: '167.172.195.92',
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+  /*
+   pool: {
+     max: 1000,
+     min: 0,
+     acquire: 1000000,
+     idle: 10000
+   }*/
 });
 
 
 const bucketConfig = {
-    bucketName: process.env.BUCKET_NAME,
-    dirName: process.env.BUCKET_PHOTO_DIR, /* optional */
-    region: process.env.BUCKET_REGION,
-    accessKeyId: process.env.BUCKET_ACCESS_KEY,
-    secretAccessKey: process.env.BUCKET_SECRET_KEY,
-    s3Url: process.env.BUCKET_URL, /* optional */
-    documentDir: process.env.BUCKET_DOCUMENT_DIR,
-    figuresDir: process.env.BUCKET_FIGURES_DIR,
-  }
+  bucketName: process.env.BUCKET_NAME,
+  dirName: process.env.BUCKET_PHOTO_DIR, /* optional */
+  region: process.env.BUCKET_REGION,
+  accessKeyId: process.env.BUCKET_ACCESS_KEY,
+  secretAccessKey: process.env.BUCKET_SECRET_KEY,
+  s3Url: process.env.BUCKET_URL, /* optional */
+  documentDir: process.env.BUCKET_DOCUMENT_DIR,
+  figuresDir: process.env.BUCKET_FIGURES_DIR,
+}
 
 
-const DEFAULT_LIMIT = 100 
+const DEFAULT_LIMIT = 100
 
 const DEFAULT_YEAR = moment(new Date()).subtract(24, 'year').format('YYYY')
 
